d93c25ea5785ebecb917acc33580a3ea5dea0755
davcs86 <davcs86@gmail.com>
2017-09-27 10:40:25 -0500

    Fixes and some refactoring

--- Demo/Form1.cs
+++ Demo/Form1.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Text;
 using System.Windows.Forms;
 using UHWID;
 
@@ -18,9 +13,8 @@ namespace Demo
 
         private void button1_Click_1(object sender, EventArgs e)
         {
-            UHWIDEngine uniqueUIDs = new UHWIDEngine();
-            textBox2.Text = uniqueUIDs.SimpleUID;
-            textBox1.Text = uniqueUIDs.AdvancedUID;
+            textBox2.Text = UHWIDEngine.SimpleUid;
+            textBox1.Text = UHWIDEngine.AdvancedUid;
         }
     }
 }
